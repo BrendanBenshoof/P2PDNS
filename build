a789b1daad40b94262885e86a7acd2d5cdeab8c6
= <=>
2013-09-19 16:05:30 -0400

    added a structure for source files added a easy build command to maintain that structure

---
+++ build
@@ -0,0 +1,2 @@
+#!/bin/bash
+javac -d bin src/*.java
---
+++ src/msg.java
@@ -0,0 +1,16 @@
+class Message
+{
+
+public int origin;
+public int dest;
+public String text;
+public Message(int origin, int dest, String text)
+    {
+        this.origin = origin;
+        this.dest = dest;
+        this.text = text;
+    }
+
+
+
+}
